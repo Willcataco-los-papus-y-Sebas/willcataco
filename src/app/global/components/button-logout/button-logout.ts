78018ca4b3a6bd4ebfce7b37bf10427ba0259f46
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2025-12-24 15:57:27 -0400

    feat: add special button for log out

---
+++ src/app/global/components/button-logout/button-logout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-logout',
+  imports: [],
+  templateUrl: './button-logout.html',
+  styleUrl: './button-logout.css',
+})
+export class ButtonLogout {
+
+}
