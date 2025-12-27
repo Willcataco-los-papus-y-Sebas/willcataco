7a987f1a04f1085de48890563132f14b6a1170bf
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2025-12-27 16:49:42 -0400

    fix: solving problems format code with prettier

--- src/app/global/components/button-logout/button-logout.ts
+++ src/app/global/components/button-logout/button-logout.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './button-logout.html',
   styleUrl: './button-logout.css',
 })
-export class ButtonLogout {
-
-}
+export class ButtonLogout {}
