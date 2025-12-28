dfd002e97665ac5c5e3348d2722c9829738cbf9e
zer4c <pabloazurduy@gmail.com>
2025-12-28 17:54:41 -0400

    feat: add basic template for dropdown

--- src/app/global/components/dropdown/dropdown.ts
+++ src/app/global/components/dropdown/dropdown.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dropdown',
-  imports: [],
+  standalone : true,
+  imports: [CommonModule],
   templateUrl: './dropdown.html',
   styleUrl: './dropdown.css',
 })
 export class Dropdown {
+  @Input() is_active = false;
+  @Input() drop_title = '';
 
+  toggle(){
+    this.is_active = !this.is_active;
+  }
 }
