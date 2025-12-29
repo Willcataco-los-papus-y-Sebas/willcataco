b8bf5ac16a96239cdca74aef38c377fbe92390ef
zer4c <pabloazurduy@gmail.com>
2025-12-28 21:37:32 -0400

    feat: header complete for dropdown component

--- src/app/global/components/dropdown/dropdown.ts
+++ src/app/global/components/dropdown/dropdown.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Chevron } from '../chevron/chevron';
 
 @Component({
   selector: 'app-dropdown',
   standalone : true,
-  imports: [CommonModule],
+  imports: [CommonModule, Chevron],
   templateUrl: './dropdown.html',
   styleUrl: './dropdown.css',
 })
 export class Dropdown {
-  @Input() is_active = false;
-  @Input() drop_title = '';
+  @Input() is_active: boolean = false;
+  @Input() drop_title: string = "Titulo";
 
   toggle(){
     this.is_active = !this.is_active;
