1b4906dd640c339d06bfabd7e66c3f1b45d4daf7
zer4c <pabloazurduy@gmail.com>
2025-12-30 21:37:39 -0400

    fix: name dropdownComponent in spec.ts file in module dropdown

--- src/app/global/components/dropdown/dropdown.spec.ts
+++ src/app/global/components/dropdown/dropdown.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Dropdown } from './dropdown';
+import { DropdownComponent } from './dropdown';
 
-describe('Dropdown', () => {
-  let component: Dropdown;
-  let fixture: ComponentFixture<Dropdown>;
+describe('DropdownComponent', () => {
+  let component: DropdownComponent;
+  let fixture: ComponentFixture<DropdownComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Dropdown],
+      imports: [DropdownComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Dropdown);
+    fixture = TestBed.createComponent(DropdownComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
