8111ab8890699ca6ff95fc71a4d162c9f8d0adfe
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2025-12-30 21:04:04 -0400

    refactor: delete button logout and menu

--- src/app/global/components/button-logout/button-logout.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-button-logout',
-  imports: [],
-  templateUrl: './button-logout.html',
-  styleUrl: './button-logout.css',
-})
-export class ButtonLogout {}
--- src/app/global/components/button-menu/button-menu.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-button-menu',
-  imports: [],
-  templateUrl: './button-menu.html',
-  styleUrl: './button-menu.css',
-})
-export class ButtonMenu {}
