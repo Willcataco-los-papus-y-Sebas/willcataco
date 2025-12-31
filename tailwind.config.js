66cad530b01c1a275fa34c659016000abc8bb651
heidyMC <heidy20mc@gmail.com>
2025-12-31 13:34:47 -0400

    chore: format code with prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // 1. Modo oscuro por defecto del sistema
-  darkMode: 'media', 
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  darkMode: 'media',
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       // 2. Paleta de colores única
@@ -22,11 +20,11 @@ module.exports = {
       },
       // 4. Tamaños de texto únicos
       fontSize: {
-        'title': ['20px', { lineHeight: '28px', fontWeight: '700' }],
-        'subtitle': ['18px', { lineHeight: '24px', fontWeight: '500' }],
-        'body': ['14px', { lineHeight: '20px', fontWeight: '400' }],
+        title: ['20px', { lineHeight: '28px', fontWeight: '700' }],
+        subtitle: ['18px', { lineHeight: '24px', fontWeight: '500' }],
+        body: ['14px', { lineHeight: '20px', fontWeight: '400' }],
       },
     },
   },
   plugins: [],
-}
+};
