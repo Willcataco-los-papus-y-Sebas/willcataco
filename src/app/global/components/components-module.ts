b7e370c9f30c97f3296b8534018e54c61b7a3b32
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2025-12-31 18:28:36 -0400

    feat: add direct access componente html

--- src/app/global/components/components-module.ts
+++ src/app/global/components/components-module.ts
@@ -4,10 +4,11 @@ import { ButtonComponent } from './button/button';
 import { ModalComponent } from './modal/modal';
 import { DropdownComponent } from './dropdown/dropdown';
 import { HeaderComponent } from './header/header';
+import { DirectAccessComponent } from './direct-access/direct-access';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent],
-  exports: [ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent],
+  imports: [CommonModule, ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent, DirectAccessComponent],
+  exports: [ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent, DirectAccessComponent],
 })
 export class ComponentsModule {}
