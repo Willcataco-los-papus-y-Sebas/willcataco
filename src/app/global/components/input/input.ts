b76e473498dd3b28c438610c04c125a405594e18
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2026-01-01 10:58:52 -0400

    fix:enable data transmission in Input Component

--- src/app/global/components/input/input.ts
+++ src/app/global/components/input/input.ts
@@ -1,17 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   templateUrl: './input.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() label = '';
   @Input() type = 'text';
   @Input() placeholder = '';
-  @Input() id: string = 'input-' + Math.random().toString(36).substring(2);
-  @Input() value = '';
+  @Input() id = 'input-' + Math.random().toString(36).substring(2);
+
+  value: string = '';
+  isDisabled: boolean = false;
+
+  onChange = (_: any) => {};
+  onTouch = () => {};
+
+  writeValue(value: any): void {
+    this.value = value || '';
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  onInput(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.value = inputElement.value;    
+    this.onChange(this.value);
+    this.onTouch();
+  }
 }
