659cf45926905a84e3b31155492a66423109add2
SebastianBarreraVargas <sebastinbarreravargascat2014@gmail.com>
2026-01-01 02:09:27 -0400

    feat: add input as component

--- src/app/global/components/components-module.ts
+++ src/app/global/components/components-module.ts
@@ -4,10 +4,11 @@ import { ButtonComponent } from './button/button';
 import { ModalComponent } from './modal/modal';
 import { DropdownComponent } from './dropdown/dropdown';
 import { HeaderComponent } from './header/header';
+import { InputComponent } from './input/input'; 
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent],
-  exports: [ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent],
+  imports: [CommonModule, ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent, InputComponent],
+  exports: [ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent, InputComponent],
 })
 export class ComponentsModule {}
