e4f6dddb3ecefa15269f98f1939faf316b2e4cd2
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2026-01-02 15:02:33 -0400

    feat(ui): create toast component

---
+++ src/app/global/components/toast/toast.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastService } from './toast.service';
+import { ToastType } from './toast.types';
+
+@Component({
+    selector: 'app-toast',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './toast.html',
+})
+export class ToastComponent {
+    toastService = inject(ToastService);
+
+    getToastClasses(type: ToastType): string {
+        switch (type) {
+            case 'success':
+                return 'bg-green-50 border-green-500 text-green-900';
+            case 'error':
+                return 'bg-red-50 border-red-500 text-red-900';
+            case 'warning':
+                return 'bg-yellow-50 border-yellow-500 text-yellow-900';
+            case 'info':
+                return 'bg-blue-50 border-blue-500 text-blue-900';
+            default:
+                return 'bg-white border-gray-200 text-gray-900';
+        }
+    }
+}
