28f8bc0e48f4e138d246810989e2946e488fa363
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2026-01-02 15:08:43 -0400

    feat(ui): integrate toast in app root

--- src/app/global/components/components-module.ts
+++ src/app/global/components/components-module.ts
@@ -4,10 +4,11 @@ import { ButtonComponent } from './button/button';
 import { ModalComponent } from './modal/modal';
 import { DropdownComponent } from './dropdown/dropdown';
 import { HeaderComponent } from './header/header';
+import { ToastComponent } from './toast/toast';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent],
-  exports: [ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent],
+  imports: [CommonModule, ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent, ToastComponent],
+  exports: [ButtonComponent, ModalComponent, DropdownComponent, HeaderComponent, ToastComponent],
 })
-export class ComponentsModule {}
+export class ComponentsModule { }
