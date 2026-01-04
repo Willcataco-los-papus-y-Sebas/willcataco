f9d26c5a9a5df559406a365f667608b72f1b7932
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2026-01-04 18:00:28 -0400

    style: reformat kebab component spec file indentation

--- src/app/global/components/kebab/kebab.spec.ts
+++ src/app/global/components/kebab/kebab.spec.ts
@@ -2,27 +2,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { KebabComponent } from './kebab';
 
 describe('KebabComponent', () => {
-    let component: KebabComponent;
-    let fixture: ComponentFixture<KebabComponent>;
+  let component: KebabComponent;
+  let fixture: ComponentFixture<KebabComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [KebabComponent],
-        }).compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [KebabComponent],
+    }).compileComponents();
 
-        fixture = TestBed.createComponent(KebabComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+    fixture = TestBed.createComponent(KebabComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-    it('should toggle menu', () => {
-        component.toggleMenu(new Event('click'));
-        expect(component.isOpen).toBe(true);
-        component.toggleMenu(new Event('click'));
-        expect(component.isOpen).toBe(false);
-    });
+  it('should toggle menu', () => {
+    component.toggleMenu(new Event('click'));
+    expect(component.isOpen).toBe(true);
+    component.toggleMenu(new Event('click'));
+    expect(component.isOpen).toBe(false);
+  });
 });
