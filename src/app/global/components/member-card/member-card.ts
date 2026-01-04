5aa63e2c213b991f805c56b34307b2b4c83ae605
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2026-01-04 17:12:43 -0400

    feat(ui): create member card component logic

---
+++ src/app/global/components/member-card/member-card.ts
@@ -0,0 +1,25 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Member } from './member.types';
+
+@Component({
+    selector: 'app-member-card',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './member-card.html',
+    styleUrl: './member-card.css',
+})
+export class MemberCardComponent {
+    @Input({ required: true }) member!: Member;
+    @Input() showActions = true;
+    @Output() actionClick = new EventEmitter<void>();
+
+    get fullName(): string {
+        return `${this.member.name} ${this.member.last_name}`;
+    }
+
+    onActionClick(event: Event) {
+        event.stopPropagation();
+        this.actionClick.emit();
+    }
+}
