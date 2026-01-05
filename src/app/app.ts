bf1d84dcc395e5f1037d6ddfc5ab2f7d626d66db
Ricardo Ramirez <ricardo@richixs.dev>
2026-01-04 21:01:37 -0400

    chore: migrate to standalone components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ComponentsModule } from './global/components/components-module';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ComponentsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
