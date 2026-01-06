942b5fd1fceda43da37f7c5a5f7bd6a12b77d1e9
Ricardo Ramirez <ricardo@richixs.dev>
2026-01-05 21:47:00 -0400

    chore: add paths and update app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from '@interceptors/auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withFetch()),
-    provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
   ],
 };
