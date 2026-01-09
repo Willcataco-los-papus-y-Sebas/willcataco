15dbf1877612c8ce5b836732e0a2025cde56e487
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2026-01-09 12:21:46 -0400

    fix: solving problems with prettier?

--- src/app/shared/models/streets/street.ts
+++ src/app/shared/models/streets/street.ts
@@ -1,15 +1,15 @@
 export interface Street {
-    id: number;
-    name: string;
-    created_at: string;
-    updated_at: string;
-    deleted_at: string | null;
+  id: number;
+  name: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
 }
 
 export interface StreetCreate {
-    name: string;
+  name: string;
 }
 
 export interface StreetUpdate {
-    name?: string;
+  name?: string;
 }
