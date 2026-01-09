f85f87b2aa578485cd3c24b920df8a8ab1c920b5
zer4c <pabloazurduy@gmail.com>
2026-01-09 17:02:14 -0400

    feat: add needed files for carrusel component and behaviour in ts

---
+++ src/app/shared/components/carrusel/carrusel.ts
@@ -0,0 +1,62 @@
+import { CommonModule } from '@angular/common';
+import { Component , Input } from '@angular/core';
+
+export interface ICarruselItem{
+  id : number,
+  title: string,
+  subtitle?: string,
+  footPage?: string,
+  marginLeft?: number,
+}
+
+
+@Component({
+  selector: 'app-carrusel',
+  imports: [CommonModule],
+  templateUrl: './carrusel.html',
+  styleUrl: './carrusel.css',
+})
+export class Carrusel {
+  @Input() items: ICarruselItem[] = [];
+  @Input() colorItem : string = "bg-background"
+
+  public currentPosition = 0;
+
+  ngOnInit() {
+    this.items.forEach((i , index) =>{
+      i.id = index;
+      i.marginLeft = 0;
+    });
+  }
+
+  setCurrentPosition(position: number) {
+    this.currentPosition = position;
+    if (this.items.length > 0) {
+      this.items[0].marginLeft = -100 * position;
+    }
+  }
+  
+  setNext(){
+    let finalPercentage = 0;
+    let nextPosition = this.currentPosition +1 ;
+    if(nextPosition < this.items.length){
+      finalPercentage = -100 * nextPosition;
+    }else{
+      nextPosition = 0;
+    }
+    this.items[0].marginLeft = finalPercentage
+    this.currentPosition = nextPosition;
+  }
+  setBack(){
+    let finalPercentage = 0;
+    let backPosition = this.currentPosition -1;
+    if (backPosition >= 0){
+      finalPercentage = -100 * backPosition;
+    }else{
+      backPosition = this.items.length -1;
+      finalPercentage = -100 * backPosition;
+    }
+    this.items[0].marginLeft = finalPercentage
+    this.currentPosition = backPosition;
+  }
+}
