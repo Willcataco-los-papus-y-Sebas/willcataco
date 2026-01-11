a84bb9d841fb4f1b15741441d4c9783d6e74acdc
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2026-01-11 11:18:48 -0400

    refactor: add api-response model

--- src/app/core/services/street/street.ts
+++ src/app/core/services/street/street.ts
@@ -2,15 +2,8 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '@envs/environment';
 import { Observable } from 'rxjs';
+import { ApiResponse } from '@models/api-response';
 import { Street, StreetCreate, StreetUpdate } from '@models/streets';
-
-export interface ApiResponse<T> {
-  detail: string;
-  status_code: number;
-  data: T;
-  limit?: number;
-  offset?: number;
-}
 
 @Injectable({
   providedIn: 'root',
