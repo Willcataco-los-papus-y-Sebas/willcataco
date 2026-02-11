6848c039ba7f7e391b7bd46af69296a3f8fd749a
Ricardo Ramirez <ricardo@richixs.dev>
2026-02-11 09:46:48 -0400

    feat(sidebar): add sidebar component

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@components/header';
 import { FooterComponent } from '@components/footer';
+import { SidebarComponent } from '@components/sidebar';
 import { HeaderService } from '@services/header';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, SidebarComponent, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
