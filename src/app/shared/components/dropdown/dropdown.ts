2349fb7372c89ce40068039243bfc6c0c47c826c
JhosuaAlejandroBustillosCalderon <jhosuabustilloscalderonq@gmail.com>
2026-03-03 23:56:46 -0400

    feat: add mobile design for dropdown with a hint of refactoring

--- src/app/shared/components/dropdown/dropdown.ts
+++ src/app/shared/components/dropdown/dropdown.ts
@@ -1,5 +1,6 @@
-import { Component, Input, output } from '@angular/core';
+import { Component, Input, output, signal, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalComponent } from '@components/modal';
 
 export interface DropdownItem {
   label: string;
@@ -9,7 +10,7 @@ export interface DropdownItem {
 @Component({
   selector: 'app-dropdown',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModalComponent],
   templateUrl: './dropdown.html',
   styleUrl: './dropdown.css',
 })
@@ -18,6 +19,8 @@ export class DropdownComponent {
   @Input() dropTitle = 'Titulo';
   @Input() items: DropdownItem[] = [];
   selectedIndex: number | null = null;
+
+  readonly isMobile = signal(window.innerWidth < 1024);
 
   itemSelected = output<string>();
 
@@ -36,5 +39,10 @@ export class DropdownComponent {
 
   isItemSelected(index: number) {
     return index === this.selectedIndex;
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile.set(window.innerWidth < 1024);
   }
 }
